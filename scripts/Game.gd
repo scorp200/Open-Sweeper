039325f8cb84d701926c8622c5cc9b740626e4c3
Anton K. (ai Doge) <abca2004@gmail.com>
2019-07-26 05:21:23 -0400

    working prototype

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var width = 20
-var height = 20
+const width = 30
+const height = 30
+const bombs = 50
 var tiles = []
 var camera: Camera2D
 var start_pos:Vector2
@@ -20,13 +21,13 @@ func _ready():
 	for y in range(height):
 		for x in range(width):
 			var i = y * width + x
-			var sprite = Tile.new()
-			size = sprite.texture.get_width()
+			var sprite = Tile.new(tiles, i, width)
+			size = sprite.getSprite().texture.get_width()
 			tiles.append(sprite)
 			$Tiles.add_child(sprite)
 			sprite.set_position(Vector2(x * size, y * size))
 			
-	for i in range(15):
+	for i in range(bombs):
 		var index = randi()%tiles.size()
 		while tiles[index].isBomb():
 			index = randi()%tiles.size()
@@ -37,16 +38,18 @@ func _process(delta):
 	if Input.action_press("WheelUp"):
 		camera.set_zoom(camera.zoom + Vector2(10, 10))
 	if Input.is_action_just_released("MouseLeft") && !dragging:
-		var x = floor(($Tiles.get_local_mouse_position().x + size / 2) / size)
-		var y = floor(($Tiles.get_local_mouse_position().y + size / 2) / size)
+		var mouse = $Tiles.get_local_mouse_position()
+		var x = floor((mouse.x + size / 2) / size)
+		var y = floor((mouse.y + size / 2) / size)
 		var i = y * width + x
-		var color: Color
-		if tiles[i].isBomb():
-			color = Color("#F57F17")
-		else:
-			color = Color("#4DD0E1")
-		tiles[i].modulate = color
-		
+		tiles[i].setRevealed()
+		tiles[i].countBombs(x, y)
+	if Input.is_action_just_released("MouseRight") && !dragging:
+		var mouse = $Tiles.get_local_mouse_position()
+		var x = floor((mouse.x + size / 2) / size)
+		var y = floor((mouse.y + size / 2) / size)
+		var i = y * width + x
+		tiles[i].setMarked()
 	if Input.is_action_pressed("MouseLeft"):
 		if !startDragging:
 			start_pos = camera.position + camera.get_local_mouse_position()
@@ -60,7 +63,7 @@ func _process(delta):
 		camera.set_position(start_pos - camera.get_local_mouse_position())
 	
 func _input(event):
-	if event.is_pressed():
+	if event.is_pressed() && event is InputEventMouse:
 		if event.button_index == BUTTON_WHEEL_UP && camera.zoom > zoom_min:
 			camera.set_zoom(camera.zoom - zoom_step)
 		elif event.button_index == BUTTON_WHEEL_DOWN && camera.zoom < zoom_max:
--- scripts/tile.gd
+++ scripts/tile.gd
@@ -1,13 +1,93 @@
-class_name Tile extends Sprite
+class_name Tile extends Node2D
 
 var _isBomb = false
+var _revealed = false
+var tiles:Array
+var index:int
+var width:int
+var count:int = -1
+var sprite: Sprite
 
-func _init():
-	texture = load("res://sprites/tile.png")
-	modulate = Color("#ef5350")
+const pattern = [Vector2(0, -1), Vector2(0, 1), Vector2(-1, 0), Vector2(1, 0)]
+func _init(tiles, index, width):
+	sprite = Sprite.new()
+	sprite.texture = load("res://sprites/tile.png")
+	sprite.modulate = Color("#ef5350")
+	add_child(sprite)
+	
+	self.tiles = tiles
+	self.index = index
+	self.width = width
+	
+	var text = Label.new()
+	var font = load("res://font/joystix.tres") as DynamicFont
+	font.size = 35
+	text.add_font_override("font", font)
+	text.align = 1
+	text.valign = 1
+	var margin = width
+	text.margin_bottom = margin
+	text.margin_top = -margin
+	text.margin_left = -margin
+	text.margin_right = margin
+	text.rect_position = Vector2(-margin, -margin)
+	text.text = "%s" % count
+	text.visible = false
+	text.name = "count"
+	add_child(text)
 	
 func isBomb():
 	return _isBomb
 	
 func setBomb():
 	_isBomb = true
+
+func setMarked():
+	if count != -1:
+		return
+	$count.text = "?"
+	$count.visible = true
+
+func setRevealed():
+	if _revealed:
+		return
+	var color: Color
+	if _isBomb:
+		color = Color("#F57F17")
+	else:
+		color = Color("#4DD0E1")
+	sprite.modulate = color
+	_revealed = true
+
+func countBombs(cx, cy):
+	if _isBomb || count != -1:
+		return count
+	count = 0
+	for x in range(3):
+		for y in range(3):
+			var ny = (cy + y - 1)
+			var nx = (cx + x - 1)
+			var i = ny * width + nx
+			if i >= 0 && i < tiles.size() && tiles[i].isBomb():
+				count += 1
+	if count == 0:
+		revealeNeighbors(cx, cy)
+	if count > 0:
+		$count.text = "%s" % count
+		$count.visible = true
+	return count
+	
+func revealeNeighbors(cx, cy):
+	for p in pattern:
+		var ny = (cy + p.y)
+		var nx = (cx + p.x)
+		var i = ny * width + nx
+		if i >= 0 && i < tiles.size() && !tiles[i].isBomb():
+			tiles[i].setRevealed()
+			if tiles[i].countBombs(nx, ny) != 0 && count > 0:
+				return
+			
+	return true
+	
+func getSprite():
+	return sprite
